8cbfc1cd796dd34b2262c6625fc91ab1e2fcc0df
janpc01 <janpc01@gmail.com>
2023-11-13 12:03:29 -0700

    Validation and error handling for user reg and login routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
@@ -10,32 +11,56 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Register User
-app.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const hash = await bcrypt.hash(password, 10); // Hash the password
-    User.createUser(username, hash); // Store user
-    res.status(201).send('User created');
-  } catch (error) {
-    res.status(500).send('Error registering new user');
-  }
-});
+app.post('/register',
+  // Validation rules as middleware
+  body('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
+  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
+
+  // Route handler
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { username, password } = req.body;
+      const hash = await bcrypt.hash(password, 10); // Hash the password
+      User.createUser(username, hash); // Store user
+      res.status(201).send('User created');
+    } catch (error) {
+      res.status(500).send('Error registering new user');
+    }
+  });
 
 // User Login
-app.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = User.findUser(username);
+app.post('/login',
+  // Validation rules as middleware
+  body('username').notEmpty().withMessage('Username is required'),
+  body('password').notEmpty().withMessage('Password is required'),
 
-    if (user && await bcrypt.compare(password, user.hash)) {
-      res.status(200).send('Login successful');
-    } else {
-      res.status(401).send('Invalid credentials');
+  // Route handler
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  } catch (error) {
-    res.status(500).send('Error logging in');
-  }
-});
+
+    try {
+      const { username, password } = req.body;
+      const user = User.findUser(username);
+
+      if (user && await bcrypt.compare(password, user.hash)) {
+        res.status(200).send('Login successful');
+      } else {
+        res.status(401).send('Invalid credentials');
+      }
+    } catch (error) {
+      res.status(500).send('Error logging in');
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
